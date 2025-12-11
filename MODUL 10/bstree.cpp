38bb43b56ec0966499392a8250f28aa714b64f53
jacquilined <nofiaaadewifitriana@gmail.com>
2025-12-11 13:36:29 +0700

    Add files via upload

---
+++ MODUL 10/bstree.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "bstree.h"
+using namespace std;
+
+address alokasi(infotype x) {
+    address p = new Node;
+    p->info = x;
+    p->left = NULL;
+    p->right = NULL;
+    return p;
+}
+
+void insertNode(address &root, infotype x) {
+    if (root == NULL) {
+        root = alokasi(x);
+    } else if (x < root->info) {
+        insertNode(root->left, x);
+    } else if (x > root->info) {
+        insertNode(root->right, x);
+    }
+}
+
+address findNode(infotype x, address root) {
+    if (root == NULL)
+        return NULL;
+    else if (x == root->info)
+        return root;
+    else if (x < root->info)
+        return findNode(x, root->left);
+    else
+        return findNode(x, root->right);
+}
+
+void inOrder(address root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        cout << root->info << " - ";
+        inOrder(root->right);
+    }
+}
---
+++ MODUL 10/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "bstree.h"
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+
+    address root = NULL;
+
+    // insert nodes
+    insertNode(root, 1);
+    insertNode(root, 2);
+    insertNode(root, 6);
+    insertNode(root, 4);
+    insertNode(root, 5);
+    insertNode(root, 3);
+    insertNode(root, 0);
+    insertNode(root, 7);
+
+    // print in-order
+    inOrder(root);
+    cout << endl;
+
+    // menahan console seperti CodeBlocks
+    system("pause");
+
+    return 0;
+}
